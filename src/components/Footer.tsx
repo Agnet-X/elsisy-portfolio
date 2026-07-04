f85a651971befd3d4fea189299ea27dd80489e96
AgnetX <Abdullahesmaeal@gmail.com>
2026-07-04 14:21:09 +0200

    Remove copyright line from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -107,9 +107,6 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-white/10 pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
-          <p className="text-white/40 text-sm">
-            &copy; {new Date().getFullYear()} {CONTENT.identity.name}. {tf.rights}
-          </p>
           <p className="text-white/40 text-sm font-serif italic">{tf.tagline}</p>
         </div>
       </div>
